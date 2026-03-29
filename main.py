a63e7acb496fe3d137e5063363430d621bf8f372
Chris Bogues <chris.bogues@gmail.com>
2026-03-28 18:21:09 -0700

    added screen height - width & FPS

--- main.py
+++ main.py
@@ -1,12 +1,43 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
-
+from logger import log_state
 def main():
     print("Starting Asteroids with pygame version: VERSION")
 
-print("Starting Asteroids...")
-print(f"Screen width: {SCREEN_WIDTH}")
-print(f"Screen height: {SCREEN_HEIGHT}")
+    print("Starting Asteroids...")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+
+# Initialize pygame
+    pygame.init()
+
+    #create the game window
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    # Create clock object and delta time variable
+    clock = pygame.time.Clock()
+    dt = 0
+
+    #game loop
+    while True:
+        # Log game state
+        log_state()
+
+        # Process pygame events
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        
+
+        # Fill screen with events
+        screen.fill("black")
+
+        #Refresh the screen
+        pygame.display.flip()
+        
+        #Limit to 60 FPS and calculate  delta time
+        dt = clock.tick(60) / 1000.0
+       # print(dt)
 
 if __name__ == "__main__":
     main()
