fc631c51b981f1ea56c313e62b10d16334173df1
Chris Bogues <chris.bogues@gmail.com>
2026-04-04 14:12:28 -0700

    Add player movement controls - WASD keys for rotation and movement

--- player.py
+++ player.py
@@ -26,6 +26,10 @@ class Player(CircleShape):
         self.rotation += PLAYER_TURN_SPEED * dt
     
     def move(self, dt):
+        unit_vector = pygame.Vector2(0, 1)
+        rotated_vector = unit_vector.rotate(self.rotation)
+        rotated_with_speed_vector = rotated_vector * PLAYER_SPEED * dt
+        self.position += rotated_with_speed_vector
         
         
         
@@ -37,4 +41,8 @@ class Player(CircleShape):
             self.rotate(-dt)
         if keys[pygame.K_d]:
             self.rotate(dt)
+        if keys[pygame.K_w]:
+            self.move(dt)
+        if keys[pygame.K_s]:
+            self.move(-dt)
 
